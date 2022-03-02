3fde39d2cb7c414ad7c5d6a7c1741c5cb24871a6
Igor Agapov <aio350@jetstyle.ru>
2022-03-02 15:07:40 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve, dirname } from 'path'
+import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+
 const _dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
